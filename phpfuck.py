7c605765d3b12193884d1bad6cfa5ff7301bd68c
黃志仁 <tbsthitw@gmail.com>
2019-11-15 02:56:25 +0800

    Remove `,` in create_function mode :)

--- phpfuck.py
+++ phpfuck.py
@@ -127,9 +127,19 @@ class PHPFuck():
         return '.'.join([f"({self.char_mapping[c]})" for c in code])
 
     def eval_code(self, code, mode="create_function"):
+        def clean_code(code):
+            return code.replace('\n', '').replace(' ', '')
+
         if mode == 'create_function':
             create_function = self.encode("create_function")
-            eval_code = f"({create_function})([].[]^[].[],{code})();"
+            str_getcsv = self.encode("str_getcsv")
+            comma = self.encode(",")
+            quote = self.encode('"')
+            eval_code = f"{comma}.{quote}.{code}.{quote}"
+            eval_code = f"""({create_function})(
+                ...({str_getcsv})({comma}.{quote}.{code}.{quote})
+            )();
+            """
 
         elif mode == 'assert':  # only support PHP < 7.1
             assert_func = self.encode('assert')
@@ -139,8 +149,8 @@ class PHPFuck():
                 ({code}).
                 ({self.encode(';return 1;})()')})
             );
-            """.replace('\n', '').replace(' ','')
-        return eval_code
+            """
+        return clean_code(eval_code)
 
 
 if __name__ == "__main__":
@@ -150,7 +160,7 @@ if __name__ == "__main__":
                         help="write encoded string into some file.")
     parser.add_argument("-P", "--plain-string", dest="plain", action='store_true',
                         help="encode as plain string (without eval it).")
-    parser.add_argument("-E", "--eval", dest="eval", 
+    parser.add_argument("-E", "--eval", dest="eval",
                         choices=['assert', 'create_function'], default='create_function',
                         help="choose eval mode. (`assert` mode only support PHP < 7.1)")
     args = parser.parse_args()
@@ -161,16 +171,16 @@ if __name__ == "__main__":
 
     encoded = phpfuck.encode(code)
     if args.eval and not args.plain:
-        encoded = "<?php " + phpfuck.eval_code(encoded, args.eval) + " ?>"
+        encoded = "<?php " + phpfuck.eval_code(encoded, args.eval) + " ?>\n"
 
     if args.file:
         open(args.file, 'w').write(encoded)
     else:
         print(encoded)
 
-    print()
     if args.plain:
         print(f"Output as plain string, without eval it.")
     else:
         print(f"Using `{args.eval}` mode to eval.")
+
     print(len(encoded), "chars.")
