7d0d20b3ab52a74ca648dc3d09f283c195a43c93
黃志仁 <tbsthitw@gmail.com>
2019-11-19 01:55:00 +0800

    Update README.md

--- examples/phpinfo.php
+++ examples/phpinfo.php
@@ -1,12 +1,3 @@
-# PHPFuck: ([+.^])
-Using only 7 different characters to write and execute php. 
-
-Only support PHP 7+ currently.
-
-## Example Code
-The following source will execute `phpinfo();`:
-
-```php
 <?php 
   ((([].[])[([]^[[]])+([]^[[]])+([]^[[]])]^(([]^[]).[])[([]^[])]^(([]^[[]])+([]^[[]]).[])[([]^[])]).(([].[])[([]^[[]])]).(([].[])[([]^[[]])+([]^[[]])+([]^[[]])
   ]^(([]^[]).[])[([]^[])]^(([]^[[]])+([]^[[]])+([]^[[]])+([]^[[]]).[])[([]^[])]).(([].[])[([]^[[]])+([]^[[]])+([]^[[]])]).(([].[])[([]^[])]^(([]^[[]])+([]^[[]]
@@ -37,44 +28,3 @@ The following source will execute `phpinfo();`:
   (([]^[]).[])[([]^[])]).(([].[])[([]^[[]])+([]^[[]])+([]^[[]])]^([].[])[([]^[[]])+([]^[[]])+([]^[[]])+([]^[[]])]^(([]^[[]]).[])[([]^[])]).(([].[])[([]^[[]])]^
   ([].[])[([]^[[]])+([]^[[]])+([]^[[]])+([]^[[]])]^(([]^[]).[])[([]^[])]).(([].[])[([]^[[]])]^([].[])[([]^[[]])+([]^[[]])+([]^[[]])]^(([]^[[]]).[])[([]^[])]))) ();
 ?>
-```
-
-## Usage
-
-```
-usage: phpfuck.py [-h] [-O FILE] [-P] [-E {assert,create_function}] code
-
-positional arguments:
-  code                  any string to encode.
-
-optional arguments:
-  -h, --help            show this help message and exit
-  -O FILE, --output-file FILE
-                        write encoded string into some file.
-  -P, --plain-string    encode as plain string (without eval it).
-  -E {assert,create_function}, --eval {assert,create_function}
-                        choose eval mode. (`assert` mode only support PHP < 7.1)
-```
-
-You can just use it like this: `python3 phpfuck.py "system('id');"`
-
-### Arguments
-- code (required)
-  - Any string or php code to encode.
-- -O, --output-file
-  - Write encoded string into some file.
-- -P, --plain-string
-  - Encode as plain string (without eval it). 
-  - With this argument, I will not wrap your code into `assert` or `create_function` to eval.
-- -E, --eval
-  - You can choose your eval mode!
-  - `create_function` mode (default)
-    - `create_function('', YOUR_CODE)();`
-  - `assert` mode
-    - Only support PHP < 7.1 (=7.0.x).
-    - `assert( '(function(){ YOUR_CODE; return 1; })()' );`
-
-## TODO
-- [ ] Support characters other than ASCII range.
-- [ ] Don't use deprecated feature. (`create_function` has been DEPRECATED)
-- [ ] Web interface.
